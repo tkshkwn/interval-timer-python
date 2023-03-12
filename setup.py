aabac4844416fbe8bfe451da4479c9e0147d887e
nyaandaa <nekotk37@gmail.com>
2023-03-12 23:55:59 +0900

    define:license

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from distutils.core import setup, Extension
 setup(
     name='interval_timer',
     version='0.1.0',
-    license='proprietary',
-    description='Module Experiment',
+    license='MIT-License',
+    description='interval-timer for python',
 
     author='tkshkwn',
     author_email='nekotk37@gmail.com',
-    url='None.com',
+    url='https://github.com/tkshkwn/interval-timer-python',
+
+    keywords="timer, periodic, interval, timerfd",
 
     ext_modules = [Extension('interval_timer', ['interval_timer_python.c', 'interval_timer.c'])]
 )
