4d9898c9fbc3cf1e3266d5121f9c06e71d311373
nyaandaa <nekotk37@gmail.com>
2023-03-13 21:48:54 +0900

    fix:setup.py long_description

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from distutils.core import setup, Extension
 
+with open('README.md', 'r') as fp:
+    readme = fp.read()
+
 setup(
     name='interval-timer-python',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT-License',
     description='interval-timer for python: Windows:waitabletimer wrapper, Linux:timerfd wrapper',
-
+    long_description=long_description,
+    long_description='text/markdown',
     author='tkshkwn',
     author_email='nekotk37@gmail.com',
     url='https://github.com/tkshkwn/interval-timer-python',
-
     keywords="timer, periodic, interval, timerfd",
-
     ext_modules = [Extension('interval_timer', ['interval_timer_python.c', 'interval_timer.c'])]
 )
